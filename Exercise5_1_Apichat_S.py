f6b0d0b553a2b78febcdf1c19b716827bfc3155e
ApichatSalee <85772371+ApichatSalee@users.noreply.github.com>
2021-06-12 15:39:53 +0700

    Create Exercise5_1_Apichat_S.py

---
+++ Exercise5_1_Apichat_S.py
@@ -0,0 +1,7 @@
+Number1 = int(input("input1 :"))
+Number2 = int(input("input2 :"))
+
+print(Number1,"+",Number2 , "=", int(Number1 + Number2))
+print(Number1,"-",Number2 , "=", int(Number1 - Number2))
+print(Number1,"*",Number2 , "=", int(Number1 * Number2))
+print(Number1,"/",Number2 , "=", int(Number1 / Number2))
