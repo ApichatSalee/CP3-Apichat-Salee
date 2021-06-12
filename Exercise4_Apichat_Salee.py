3775d25b676052e8b125e09dc16df4a9ae4d9550
ApichatSalee <85772371+ApichatSalee@users.noreply.github.com>
2021-06-12 14:04:04 +0700

    Create Exercise4_Apichat_Salee.py

---
+++ Exercise4_Apichat_Salee.py
@@ -0,0 +1,11 @@
+Foundation_English = 90
+General_Business = 80
+Introduction_to_Computer_Systems = 99
+Computer_Programming = 79
+
+print("--- Your Score ---")
+print("Foundation English :" , Foundation_English )
+print("General Business :" , General_Business )
+print("Introduction to Computer Systems :" , Introduction_to_Computer_Systems )
+print("Computer Programming :" , Computer_Programming )
+
